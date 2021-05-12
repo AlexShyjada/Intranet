bd7308dcaa83d8274cb9b35a9eb24c82ed07aa88
Alexandre Shyjada Sousa <alexandresousa@salturssa.pms.ad>
2021-05-12 13:27:53 -0300

    Finalizando heder responsivo

---
+++ site/intranet-saltur/script/scripts.js
@@ -0,0 +1,17 @@
+const headerMobile = {
+  open(){
+    document
+      .querySelector('.menu-section')
+      .classList
+      .toggle('on')
+  }
+}
+
+const dropdownContent = {
+  open(){
+    document
+      .querySelector('.dropdown-content')
+      .classList
+      .toggle('on')
+  }
+}

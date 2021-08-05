c5244d39abee84623c9405d189fea13132df9aec
Alexandre Shyjada Sousa <alexandresousa@salturssa.pms.ad>
2021-08-05 16:56:14 -0300

    add components

---
+++ project/src/components/Aniversariantes/Content.ts
@@ -0,0 +1,35 @@
+interface aniversarianteData{
+  key: number;
+  id: number;
+  name: string;
+  sexo: string;
+  date: string;
+  sector: string;
+}
+
+export const content: aniversarianteData[] = [
+  {
+    key: 1,
+    id: 1,
+    sexo: "m",
+    name: "Alex Saldanha",
+    date: "08 de Agosto",
+    sector: "Contratos e Convenhos"
+  },
+  {
+    key: 2,
+    id: 2,
+    sexo: "f",
+    name: "Carine Bomfim",
+    date: "08 de Agosto",
+    sector: "Eventos"
+  },
+  {
+    key: 3,
+    id: 3,
+    sexo: "f",
+    name: "Tereza Ferreira",
+    date: "26 de Agosto",
+    sector: "GERFIM"
+  }
+]
--- project/src/components/Hero/Hero.tsx
+++ project/src/components/Hero/Hero.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import './Hero.css'
 
 export default function Hero() {
   return (
     <section>
-      <div>
-        <img src="" alt="" />
+      <div className="imgHeroSection">
+        <img className="imgHero" src="./assets/Hero/imgHero.jpg" alt="" />
       </div>
-      <div>
-        <article>
-          <h1>Recadastramento dos aniversariantes do mês de agosto</h1>
-          <p>Atenção! Está fazendo aniversário esse mês? Então não se esqueça de fazer o recadastramento na plataforma da Secretaria Municipal de Gestão - SEMGE</p>
-          <a className="btnHero" href="https://www.recadastramento.salvador.ba.gov.br/ativo/login"></a>
+      <div className="articleHero">
+        <article className="article container">
+          <h1 className="titleHero">Recadastramento dos aniversariantes do mês de agosto</h1>
+          <p className="paragrathHero">Atenção! Está fazendo aniversário esse mês? Então não se esqueça de fazer o recadastramento na plataforma da Secretaria Municipal de Gestão - SEMGE</p>
+          <a className="btnHero" href="https://www.recadastramento.salvador.ba.gov.br/ativo/login">Saiba Mais</a>
         </article>
       </div>
     </section>
